3fb1d9041ccfa460cf67e0af1d5f32025374e1bd
Tommy <4850853+wx00@users.noreply.github.com>
2021-01-17 23:40:27 +0800

    add mtime in abp format file

--- convert-abp-format.py
+++ convert-abp-format.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 from argparse import ArgumentParser
 
 from main import reformat, get_online_list
@@ -24,19 +26,27 @@ def parse_args():
     )
     return parser.parse_args()
 
+
 def main():
     args = parse_args()
     try:
+        LOCAL_FLAG = True
         with open(args.input, 'r', encoding='utf8') as fp:
             result = reformat(fp, ABP_ALLOW_FMT)
     except IOError:
+        LOCAL_FLAG = False
         print('Unable to open rule file, trying to get online list...')
         result = get_online_list(ABP_ALLOW_FMT)
 
-    content = ''.join(result)
-    with open(args.output, mode='w', encoding='utf8') as fp:
-        fp.write(content)
+    if LOCAL_FLAG:
+        last_modified = datetime.utcfromtimestamp(os.path.getmtime(args.input))
+    else:
+        last_modified = datetime.utcnow()
+    with open(args.output, mode='w', encoding='utf8', newline='\n') as fp:
+        fp.write(f'! Last Modified: {last_modified.isoformat()}\n\n')
+        fp.writelines(result)
     print('Done!')
+
 
 if __name__ == '__main__':
     main()
